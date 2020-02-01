adc6836e9d1d424c7e9d79ee7969fab04034c31f
Kasey Arnold <kaseyarnold15@gmail.com>
2020-02-01 04:19:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -44,20 +44,8 @@ end
 
 def dennis_ritchies_language
 	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+ 		
+ 		
 end
 
 def adding_matz
